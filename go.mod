35fe1e77d25b2ed1c0ee3bb68ab15a317400a649
Carolyn Van Slyck <me@carolynvanslyck.com>
2020-12-20 20:49:33 -0600

    reimage

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/carolynvs/rollersk8s
+
+go 1.15
+
+require (
+	github.com/pkg/errors v0.8.1
+	github.com/spf13/cobra v1.1.1
+)
---
+++ reimage.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"os"
+	"path/filepath"
+
+	"github.com/pkg/errors"
+	"github.com/spf13/cobra"
+)
+
+type Node struct {
+	MACAddress string
+	Leader     bool
+}
+
+var nodes = map[string]Node{
+	"rainbowdash":     {MACAddress: "01-f4-4d-30-6d-56-0c", Leader: true},
+	"twilightsparkle": {MACAddress: "01-f4-4d-30-6d-48-f6"},
+	"applejack":       {MACAddress: "01-f4-4d-30-6d-48-08"},
+	"fluttershy":      {MACAddress: "01-f4-4d-30-6d-5e-6c"},
+	"pinkiepie":       {MACAddress: "01-f4-4d-30-6d-61-51"},
+}
+
+func main() {
+	var node string
+	var complete bool
+	cmd := cobra.Command{
+		Use: "reimage",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			return reimage(node, complete)
+		},
+	}
+	cmd.Flags().StringVarP(&node, "node", "n", "", "Node hostname")
+	cmd.Flags().BoolVarP(&complete, "complete", "c", false, "Indicates if the restore is complete")
+
+	cmd.Execute()
+}
+
+func reimage(name string, complete bool) error {
+	node, ok := nodes[name]
+	if !ok {
+		return errors.Errorf("Invalid node: %s", name)
+	}
+
+	pxeDir := "/var/ftpd/pxelinux.cfg"
+	pxeFile := filepath.Join(pxeDir, node.MACAddress)
+
+	if complete {
+		err := os.Remove(pxeFile)
+		if !os.IsNotExist(err) {
+			return errors.Wrapf(err, "could not remove %q", pxeFile)
+		}
+		return nil
+	}
+
+	role := "follower"
+	if node.Leader {
+		role = "leader"
+	}
+	roleFile := filepath.Join(pxeDir, role)
+	err := os.Symlink(roleFile, node.MACAddress)
+	return errors.Wrapf(err, "Could not create pxe config file %s pointing to %s", node.MACAddress, role)
+}
